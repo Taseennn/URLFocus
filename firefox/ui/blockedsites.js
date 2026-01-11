df03cbdca4cdcdec2629a0a51c69c1f8899de1c1
Taseen <t.ghost.t13@gmail.com>
2026-01-11 22:48:04 +0000

    Add save and removal buttons for url firefox

--- firefox/ui/blockedsites.js
+++ firefox/ui/blockedsites.js
@@ -4,11 +4,33 @@ document.getElementById('return').addEventListener('click', () => {
   window.location.href = "./popup.html";
 });
 
+
+// load blocked sites from storage and display them
 async function loadBlockedSites() {
   const { blockedSites } = await extensionAPI.storage.local.get({ blockedSites: [] });
+  let id = 0;
   document.getElementById('blockedSites').innerHTML =
-    blockedSites.map(site => `<li>${site}</li>`).join('');
+    blockedSites.map((site, i) => `<li><span>${site}</span> <button id="remove=${i}">remove</button></li>`).join('');
+
 }
+
+// remove site when the remove button is clicked
+document.getElementById('blockedSites').addEventListener('click',async (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const id = event.target.id.split('=')[1];
+    const { blockedSites } = await extensionAPI.storage.local.get({ blockedSites: [] });
+    blockedSites.splice(id, 1);
+    await extensionAPI.storage.local.set({ blockedSites });
+    loadBlockedSites();
+  }
+});
+
+// save blocked sites when the save button is clicked
+document.getElementById('save').addEventListener('click', async () => {
+  const listItems = document.querySelectorAll('#blockedSites li span');
+  const blockedSites = Array.from(listItems).map(span => span.textContent.trim());
+  await extensionAPI.storage.local.set({ blockedSites });
+});
 
 loadBlockedSites();
 
